fda0029c746716933551c2f65a53bf46c105d8e9
fla-anto <flavio.antoci@gmail.com>
2026-03-25 18:40:46 +0100

    Use environment variable for server port

--- server.py
+++ server.py
@@ -148,4 +148,5 @@ async def analyze(
 
 
 if __name__ == "__main__":
-    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("server:app", host="0.0.0.0", port=port)
